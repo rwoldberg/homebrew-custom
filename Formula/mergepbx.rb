7bfd9e69c0ebbda7832d8f33f2f6fde63b7b0358
Robert Woldberg <robert@woldberg.com>
2018-12-27 11:44:10 -0700

    Fix indentation

--- Formula/mergepbx.rb
+++ Formula/mergepbx.rb
@@ -3,12 +3,11 @@ class Mergepbx < Formula
     homepage "https://github.com/rwoldberg/mergepbx"
     url "https://github.com/rwoldberg/mergepbx/archive/v0.11.tar.gz"
     sha256 "8a7ddf7879a31922f20ab496f14423b61718b5aca8f239efc49946b56678ab49"
-end
-
-def install
-    system "./build.py"
-    bin.install "mergepbx"
-end
+    
+    def install
+        system "./build.py"
+        bin.install "mergepbx"
+    end
 
 end
 
