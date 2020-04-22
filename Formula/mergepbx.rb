68f26610acaedc00f2991fbc6aa03aa9c542697f
Robert Woldberg <robert.woldberg@sling.com>
2020-04-22 08:38:51 -0600

    Update version

--- Formula/mergepbx.rb
+++ Formula/mergepbx.rb
@@ -1,8 +1,8 @@
 class Mergepbx < Formula
     desc "Merge XCode project files in git"
     homepage "https://github.com/rwoldberg/mergepbx"
-    url "https://github.com/rwoldberg/mergepbx/archive/v0.11.tar.gz"
-    sha256 "8a7ddf7879a31922f20ab496f14423b61718b5aca8f239efc49946b56678ab49"
+    url "https://github.com/rwoldberg/mergepbx/archive/v0.12.tar.gz"
+    sha256 "096d4d9a91f4204b1a2114f8e48c34e0e326714dc658e14128785eeef43a50db"
     
     def install
         system "./build.py"
